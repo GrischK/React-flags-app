35d8999879f44220de9837f5b2242ae26ad58223
GrischK <grischka.gorski@gmail.com>
2022-04-19 21:53:29 +0200

    adding text to cards and css

--- React flags app/src/Components/Cards.jsx
+++ React flags app/src/Components/Cards.jsx
@@ -1,6 +1,7 @@
 export default function Card({country}) {
   return (
     <li className="card">
+        <img src={country.flags.svg} alt={`drapeau ${country.translations.fra.common}`}/>
       <div className="infos">
         <h2>{country.translations.fra.common}</h2>
       </div>
